383818b2cd1619ce9287e65e5b3c29f949ac7801
just-hms <alessandroversari1@gmail.com>
2023-03-28 18:51:58 +0200

    kek

--- svelte/src/routes/+layout.svelte
+++ svelte/src/routes/+layout.svelte
@@ -1,28 +1,64 @@
 <script lang="ts">
     import "../app.css";
     import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte'
+  	import { Footer, FooterCopyright, FooterLink, FooterLinkGroup } from "flowbite-svelte"
 </script>
   
 
 <Navbar let:hidden let:toggle>
     <NavBrand href="/">
-      <img
-        src="https://flowbite.com/docs/images/logo.svg"
-        class="mr-3 h-6 sm:h-9"
-        alt="Flowbite Logo"
-      />
-      <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
-        Flowbite
+    <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="mr-3 h-6 sm:h-9" viewBox="0 0 16 16">
+        <path fill-rule="evenodd" d="M9 2.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1h-.39l1.4 7a2.5 2.5 0 1 1-.98.195l-.189-.938-2.43 3.527A.5.5 0 0 1 9.5 13H4.95a2.5 2.5 0 1 1 0-1h4.287l2.831-4.11L11.09 3H9.5a.5.5 0 0 1-.5-.5ZM3.915 12a1.5 1.5 0 1 0 0 1H2.5a.5.5 0 0 1 0-1h1.415Zm8.817-.789A1.499 1.499 0 0 0 13.5 14a1.5 1.5 0 0 0 .213-2.985l.277 1.387a.5.5 0 0 1-.98.196l-.278-1.387Z"/>
+        </svg>
+      <span class="hidden md:block self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+        Little Vespa House
       </span>
     </NavBrand>
-    <NavHamburger on:click={toggle} />
+
+    <!-- <NavHamburger on:click={toggle} />
     <NavUl {hidden}>
       <NavLi href="/" active={true}>Home</NavLi>
       <NavLi href="/about">About</NavLi>
       <NavLi href="/services">Services</NavLi>
       <NavLi href="/pricing">Pricing</NavLi>
       <NavLi href="/contact">Contact</NavLi>
-    </NavUl>
+    </NavUl> -->
 </Navbar>
 
-<slot />
+<div class="container mx-auto px-4 text-center items-center justify-items-center py-5">
+    <slot />
+</div>
+
+
+<Footer>
+	<FooterCopyright href="/" by="Ricky Marinsalda" year={2023} />
+	<FooterLinkGroup ulClass="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
+    <FooterLink href="https://github.com/rickymarinsalda">
+      <div class="flex items-center">
+        made by
+        <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="ml-1 w-6 h-6" viewBox="0 0 16 16">
+          <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"/>
+        </svg>
+      </div>
+    </FooterLink>
+  </FooterLinkGroup>
+  <FooterLinkGroup ulClass="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
+    <FooterLink href="tel:+393346262154">
+      <div class="flex items-center">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="mr-1 w-6 h-6" viewBox="0 0 16 16">
+          <path fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z"/>
+        </svg>
+        +39 334 6262 154
+      </div>
+    </FooterLink>
+
+    <FooterLink href="mailto:pecchiaf@tiscali.it">
+      <div class="flex items-center">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="mr-1 w-6 h-6" viewBox="0 0 16 16">
+          <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414.05 3.555ZM0 4.697v7.104l5.803-3.558L0 4.697ZM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586l-1.239-.757Zm3.436-.586L16 11.801V4.697l-5.803 3.546Z"/>
+        </svg>
+        pecchiaf@tiscali.it
+      </div>
+    </FooterLink>
+	</FooterLinkGroup>
+</Footer>
