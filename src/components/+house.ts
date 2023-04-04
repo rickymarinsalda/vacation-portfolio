322d14dc582c4557daf845ac434924c2d55e4c9e
just-hms <alessandroversari1@gmail.com>
2023-04-04 18:34:34 +0200

    fix: images path

--- src/components/+house.ts
+++ src/components/+house.ts
@@ -2,21 +2,21 @@ export const houseImages = [
     {
         id: 0,
         name: 'House',
-        imgurl: '/images/given/11.jpg'
+        imgurl: './images/given/11.jpg'
     },
     {
         id: 1,
         name: 'House',
-        imgurl: '/images/given/2.jpg'
+        imgurl: './images/given/2.jpg'
     },
     {
         id: 2,
         name: 'House',
-        imgurl: '/images/given/3.jpg'
+        imgurl: './images/given/3.jpg'
     },
     {
         id: 3,
         name: 'House',
-        imgurl: '/images/given/8.jpg'
+        imgurl: './images/given/8.jpg'
     },
 ];
