aa46219be220576c46772ad410873e00c6feef16
just-hms <alessandroversari1@gmail.com>
2023-04-04 18:34:05 +0200

    fix: images path

--- src/components/+gallery.ts
+++ src/components/+gallery.ts
@@ -1,30 +1,30 @@
 export const galleryImages = [
     {
-        src: '/images/given/1.jpg',
+        src: './images/given/1.jpg',
         alt: 'image1.jpg'
     },
     {
-        src: '/images/given/3.jpg',
+        src: './images/given/3.jpg',
         alt: 'image3.jpg'
     },
     {
-        src: '/images/given/4.jpg',
+        src: './images/given/4.jpg',
         alt: 'bathroom4.jpg'
     },
     {
-        src: '/images/given/5.jpg',
+        src: './images/given/5.jpg',
         alt: 'bathroom5.jpg'
     },
     {
-        src: '/images/given/6.jpg',
+        src: './images/given/6.jpg',
         alt: 'bathroom6.jpg'
     },
     {
-        src: '/images/given/7.jpg',
+        src: './images/given/7.jpg',
         alt: 'bathroom7.jpg'
     },
     {
-        src: '/images/given/9.jpg',
+        src: './images/given/9.jpg',
         alt: 'bathroom9.jpg'
     },
     
--- src/components/+scooter.ts
+++ src/components/+scooter.ts
@@ -2,16 +2,16 @@ export const scooterImages = [
     {
         id: 0,
         name: 'Scooter',
-        imgurl: '/images/scooter/1.jpg'
+        imgurl: './images/scooter/1.jpg'
     },
     {
         id: 1,
         name: 'Scooter',
-        imgurl: '/images/scooter/2.jpg'
+        imgurl: './images/scooter/2.jpg'
     },
     {
         id: 2,
         name: 'Scooter',
-        imgurl: '/images/scooter/3.jpg'
+        imgurl: './images/scooter/3.jpg'
     }
 ];
